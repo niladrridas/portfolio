c22702a4623beaa6472bada1f283dbab24eb7502
Niladri Das <niladri.das@lpu.in>
2024-01-18 18:41:47 +0530

    Update extract_link_text.py

--- extract_link_text.py
+++ extract_link_text.py
@@ -1,11 +1,9 @@
-# extract_link_text.py
-
 html_code = '<li><a href="https://github.com/niladrigithub">GitHub</a></li>'
 start_index = html_code.find('">') + 2
 end_index = html_code.find('</a>')
 link_text = html_code[start_index:end_index]
 
-# Capitalize the first letter
-capitalized_link_text = link_text.capitalize()
+# Capitalize the first letter while maintaining the original case
+capitalized_link_text = link_text.title()
 
 print(capitalized_link_text)
