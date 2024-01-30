3f650d7445275003c7a785793acf171fa2b110ad
Niladri Das <niladri.das@lpu.in>
2024-01-31 02:36:00 +0530

    Delete extract_link_text.py

--- extract_link_text.py
+++
@@ -1,9 +0,0 @@
-html_code = '<li><a href="https://github.com/niladrigithub">GitHub</a></li>'
-start_index = html_code.find('">') + 2
-end_index = html_code.find('</a>')
-link_text = html_code[start_index:end_index]
-
-# Capitalize the first letter while maintaining the original case
-capitalized_link_text = link_text.title()
-
-print(capitalized_link_text)
